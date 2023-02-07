1c07addcb2aa04766787a52d3101c9f16f1e03de
BibiiXD <gabimoliveiraah@gmail.com>
2023-02-07 15:16:48 +0000

    salvando o arquivo exemplo variaveis

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,19 @@
 public class Variaveis {
-
-}
+    public static void main(String[] args) {
+ int idade=16;
+ char letra='M';
+ boolean vivo=false; 
+ byte quantidadeDeBolas=7;
+ short alunosPalmital=10000;
+ double salario=1302.00;
+ float salarioMenor=1212.00f;
+ long populacaoPalmital=22000;
+ System.out.println(idade);
+ System.out.println(letra);
+ System.out.println(vivo);
+ System.out.println(quantidadeDeBolas);
+ System.out.println(alunosPalmital);
+ System.out.println(salario);
+ System.out.println(salarioMenor);
+ System.out.println(populacaoPalmital);
+}}
