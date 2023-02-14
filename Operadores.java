1554d977f44a7c553c53672fbffa30b916f7af46
BibiiXD <gabimoliveiraah@gmail.com>
2023-02-14 14:12:14 +0000

    Operadores

--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,13 @@
 public class Operadores {
-    
+    public static void main(String[] Gabi){
+
+        int idade;
+        idade = 16;
+        idade++;
+
+        System.out.println("Eu tenho " + idade + " anos ");
+
+        idade+=1;
+        System.out.println(idade);
+    }
 }
