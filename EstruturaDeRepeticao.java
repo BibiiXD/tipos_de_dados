e520473843819dc96e5872354ea08077961ee0df
BibiiXD <gabimoliveiraah@gmail.com>
2023-02-17 10:14:53 +0000

    exercicios

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,41 +1,41 @@
 public class EstruturaDeRepeticao {
 
-public static void main(String args[]){
-    int soma=0;
-    int fatorial=1;
-    //for(int i=0; i<17; i+=2){
-       // System.out.println(i);
-   // }
+  public static void main(String args[]) {
+    int soma = 0;
+    int fatorial = 1;
+    // for(int i=0; i<17; i+=2){
+    // System.out.println(i);
+    // }
 
-   //for (int i=150; i<=300; i++){
-    //System.out.println(i);
-  // }
+    // for (int i=150; i<=300; i++){
+    // System.out.println(i);
+    // }
 
     // for (int i=1; i<=1000; i++){
-       // soma = soma + i;
-   // }System.out.println(soma);
+    // soma = soma + i;
+    // }System.out.println(soma);
 
-   //for (int i=3; i<100; i+=3){
-    //System.out.println(i);
-  // }
+    // for (int i=3; i<100; i+=3){
+    // System.out.println(i);
+    // }
 
-    //for (int n=1; n<=10; n++){
-    //    System.out.println(n);
-   // }
-int fatResult = 1;
-   for (int i=0; i>1; i--){
-    if(i ==0){
-        fatResult = 1;
-        break;
-    }else{
-        fatResult = i * fatResult;
+    // for (int n=1; n<=10; n++){
+    // System.out.println(n);
+    // }
+
+    int fatResult = 1;
+    for (int j = 0; j <= 10; j++) {
+      for (int i = j; i >= 1; i--) {
+        if (i == 0) {
+          fatResult = 1;
+          break;
+        } else {
+          fatResult = i * fatResult;
+        }
+      }
+      // System.out.println(fatResult);
+      System.out.println("O Fatorial de " + j + "=" + fatResult);
+      fatResult = 1;
     }
-   }
-   System.out.println(fatResult);
-
-
-
-
-
-}
+  }
 }
